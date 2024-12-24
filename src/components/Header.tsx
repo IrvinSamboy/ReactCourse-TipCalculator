0e51eb85926bdc2a118ff8a6b28613125917428f
Irvin Samboy <contacoirvinsamboy@gmail.com>
2024-12-23 21:35:50 -0400

    Making header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header className='w-full p-4 bg-[#26CEB7]'>
+        <h1 className='text-center text-4xl font-black'>Calculadora de Propinas y Consumo</h1>
+    </header>
+  )
+}
