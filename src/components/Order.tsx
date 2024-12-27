5e1a45ef589d388b035eb8668857e900645b107e
Irvin Samboy <contacoirvinsamboy@gmail.com>
2024-12-27 12:30:52 -0400

    Creating a message if there are no orders

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -9,22 +9,27 @@ export default function Order({order, deleteItem} : OrderPropsI) {
     return (
         <div className=" px-2">
             {
-                order.map((item) => (
-                    <div className="flex justify-between py-6 items-center">
-                        <div className="font-semibold">
-                            <p>{item.name} - ${item.price}</p>
-                            <p className="font-black">Cantidad: {item.quantity} - ${item.quantity * item.price}</p>
-                        </div>
-                        <p 
-                            className="bg-red-500 text-white px-[11.5px] py-1 rounded-full cursor-pointer" 
-                            onClick={() => deleteItem(item.id)}
-                        >X</p>
-                    </div>
-                ))
+                order.length > 0? 
+                <>
+                    {
+                        order.map((item) => (
+                            <div className="flex justify-between py-6 items-center">
+                                <div className="font-semibold">
+                                    <p>{item.name} - ${item.price}</p>
+                                    <p className="font-black">Quantity: {item.quantity} - ${item.quantity * item.price}</p>
+                                </div>
+                                <p 
+                                    className="bg-red-500 text-white px-[11.5px] py-1 rounded-full cursor-pointer" 
+                                    onClick={() => deleteItem(item.id)}
+                                >X</p>
+                            </div>
+                        ))
+                    }
+                    <button className="text-white bg-black w-full p-3 font-bold mt-5">SAVE ORDER</button>
+                </>
+                : <p className="text-xl text-center">You have no orders</p>
             }
-            
-            
-            <button className="text-white bg-black w-full p-3 font-bold mt-5">SAVE ORDER</button>
+
         </div>
     )
 }
